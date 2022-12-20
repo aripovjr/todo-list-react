5f98014966bb79a9283aaa3b81bb5959f9ea94f2
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2022-12-20 16:49:46 +0100

    Adding styles

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -26,9 +26,9 @@ function Todo({todos, completeTodo, removeTodo, updateTodo}) {
     }
 
   return todos.map((todo, index)=>(
-        <div className={todos.isComplete? 'todo-row complete' : 'todo-row' } 
+        <div className={todo.isComplete? 'todo-row complete' : 'todo-row' } 
             key={index}>
-                <div key={todo.id} onClick={()=>{
+                <div className='todo-text' key={todo.id} onClick={()=>{
                     completeTodo(todo.id)
                 }}>
                     {todo.text}
